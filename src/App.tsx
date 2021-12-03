12d34753496133a9af054dbb8f9c3e8943c8d5a1
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-12-03 17:03:08 -0500

    feat(app): add react-router and config routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AuthProvider } from './context/AuthProvider';
+import { Routes, Route } from 'react-router-dom';
+import RequireAuth from './components/RequireAuth';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route
+          path='/home'
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        ></Route>
+      </Routes>
+    </AuthProvider>
   );
 }
 
