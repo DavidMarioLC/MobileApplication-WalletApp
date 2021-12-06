6a35d0ba4e603db6003b23452b901c5cb5624e45
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-12-05 20:20:04 -0500

    refact(app): add route to welcome

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { AuthProvider } from './context/AuthProvider';
 import { Routes, Route } from 'react-router-dom';
 import RequireAuth from './components/RequireAuth';
+import Welcome from './components/Welcome';
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
@@ -9,6 +10,7 @@ function App() {
   return (
     <AuthProvider>
       <Routes>
+        <Route path='/' element={<Welcome />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route
