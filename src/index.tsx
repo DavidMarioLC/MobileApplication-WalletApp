60882b1603e4512e4a09d679141742f17d332aef
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-12-05 20:20:22 -0500

    feat(app): add provider theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyles, blueTheme } from './themes/ThemeConfig';
+import { ThemeProvider } from 'styled-components';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={blueTheme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
